c5022d7b99500fa1c56ced139a15c8fcee1088f5
stoleckipawel <46271506+stoleckipawel@users.noreply.github.com>
2025-05-01 16:54:03 +0200

    Example resources

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -14,6 +14,44 @@ int main()
 	{
 		D3D12Window::Get().SetFullScreen(true);
 
+		const char* hello = "Hello World!";
+
+		// CPU BUFFER 
+		D3D12_HEAP_PROPERTIES heapUploadProperties = {};
+		heapUploadProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
+		heapUploadProperties.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+		heapUploadProperties.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+		heapUploadProperties.CreationNodeMask = 0;
+		heapUploadProperties.VisibleNodeMask = 0;
+
+		//GPU Buffer
+		D3D12_HEAP_PROPERTIES heapDefaultProperties = {};
+		heapDefaultProperties.Type = D3D12_HEAP_TYPE_DEFAULT;
+		heapDefaultProperties.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+		heapDefaultProperties.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+		heapDefaultProperties.CreationNodeMask = 0;
+		heapDefaultProperties.VisibleNodeMask = 0;
+
+		D3D12_RESOURCE_DESC resourceDesc = {};
+		resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		resourceDesc.Alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+		resourceDesc.Width = 1024; 
+		resourceDesc.Height = 1;
+		resourceDesc.DepthOrArraySize = 1;
+		resourceDesc.MipLevels = 1;
+		resourceDesc.Format = DXGI_FORMAT_UNKNOWN;
+		resourceDesc.SampleDesc.Count = 1;
+		resourceDesc.SampleDesc.Quality = 0;
+		resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+		resourceDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+
+		ComPointer<ID3D12Resource2> uploadBuffer, vertexBuffer;
+		D3D12Context::D3D12Context::Get().GetDevice()->CreateCommittedResource(&heapDefaultProperties, D3D12_HEAP_FLAG_NONE,
+			&resourceDesc, D3D12_RESOURCE_STATE_COMMON, nullptr, IID_PPV_ARGS(&uploadBuffer));
+
+		D3D12Context::D3D12Context::Get().GetDevice()->CreateCommittedResource(&heapDefaultProperties, D3D12_HEAP_FLAG_NONE,
+			&resourceDesc, D3D12_RESOURCE_STATE_COMMON, nullptr, IID_PPV_ARGS(&vertexBuffer));
+
 		while (!D3D12Window::Get().GetShouldClose())
 		{
 			// Process pending window messages
@@ -28,7 +66,7 @@ int main()
 
 			//Begin Drawing
 			auto cmdList = D3D12Context::Get().InitializeCommandList();
-
+			 
 			//Draw To Window
 			D3D12Window::Get().BeginFrame(cmdList);
 
