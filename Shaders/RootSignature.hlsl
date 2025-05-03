273b9d62c52c4f58b2df16d5260dc8b8e470aaf9
stoleckipawel <46271506+stoleckipawel@users.noreply.github.com>
2025-05-03 17:47:10 +0200

    texutres in hlsl

--- Shaders/RootSignature.hlsl
+++ Shaders/RootSignature.hlsl
@@ -3,11 +3,17 @@
 /*
 * Root Signature Layout(CPU):
 * -> - 0: float3 "Color"
+* -> - 1: descriptor table "Textures"
 * Root Signature Layout(GPU):
 * -> - b0: float3 "Color"
+* -> - t0: Texture2D<float4> "Textures"
 */
 
 
 #define ROOTSIG \
 "RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT)," \
-"RootConstants(num32BitConstants=3, b0)"
+"RootConstants(num32BitConstants=3, b0)" \
+"DescriptorTable(" \
+    "SRV(t0, numDescriptors = unbounded)" \
+")"\
+"StaticSampler(s0)"
