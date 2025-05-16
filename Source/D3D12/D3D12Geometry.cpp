dc6a8b996d21b44c0125f0da63c6d0b15262540e
stoleckipawel <46271506+stoleckipawel@users.noreply.github.com>
2025-05-16 21:47:26 +0200

    Working color interpolation!

--- Source/D3D12/D3D12Geometry.cpp
+++ Source/D3D12/D3D12Geometry.cpp
@@ -78,10 +78,9 @@ void D3D12Geometry::Upload()
 {
 	Vertex verts[] =
 	{
-		// T1
-		{{-1.0f, -1.0f, 0.0f}, {1.0, 1.0, 1.0, 1.0}},
-		{{0.0f,  1.0f,  0.0f}, {1.0, 1.0, 1.0, 1.0}},
-		{{1.0f, -1.0f,  0.0f}, {1.0, 1.0, 1.0, 1.0}}
+		{{-1.0f, -1.0f, 0.0f}, {1.0, 0.0, 1.0, 1.0}},
+		{{0.0f,  1.0f,  0.0f}, {0.0, 1.0, 1.0, 1.0}},
+		{{1.0f, -1.0f,  0.0f}, {1.0, 1.0, 0.0, 1.0}}
 	};
 
 	uint32_t vertsDataSize = sizeof(Vertex) * _countof(verts);
@@ -103,7 +102,7 @@ std::vector<D3D12_INPUT_ELEMENT_DESC> D3D12Geometry::GetVertexLayout()
 {
 	return
 	{
-		{ "POSITION",  0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "POSITION",  0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
 		{ "COLOR",  0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
 	};
 }
